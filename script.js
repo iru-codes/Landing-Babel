351a4f50bba0d37b2cac7502367d9bf50dc71aad
iru-codes <aixairupe992@gmail.com>
2025-04-07 14:40:06 -0300

    Agregado archivo de javascript con menu hamburguesa y init swiper + adaptación responsive de menú, banner superior y carrusel

---
+++ script.js
@@ -0,0 +1,34 @@
+const toggleBtn = document.querySelector('.menu_toggle');
+const menu = document.querySelector('.menu');
+
+toggleBtn.addEventListener('click', () => {
+    toggleBtn.classList.toggle('open');
+    menu.classList.toggle('open');
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Swiper(".insight-carousel", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    centeredSlidesBounds: true,
+    spaceBetween: 16,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    breakpoints: {
+      1200: {
+        navigation: false,
+        pagination: false,
+      },
+    },
+  });
+});
+  
+
+  
